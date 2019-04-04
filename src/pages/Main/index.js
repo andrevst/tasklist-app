be60efbb31b53c2489d662851cd957d86dbd703f
Andre Tonelli <aveigas@br.ibm.com>
2019-04-04 04:49:39 -0300

    Revert "Add react-table and render get/task data"
    This reverts commit a88948b2e9ff1e21c0fa9e7af68127cb2e301b88.

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,56 +1,9 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
-
-import ReactTable from "react-table";
-
 import api from "../../services/api";
-
-import { columns } from "./utils"
-
-class Main extends Component {
-
-  state = {
-    tasks: [ ],
-};
-
-handleNewTask = async e => {
-
-  const content = this.state.newTask;
-
-  await api.post('task', { content,})
-
-  this.setState({ newTask: '' });
-  
-};
-
-  async componentDidMount()  {
-      try {
-        const response = await api.get("task");
-        this.setState({ tasks: response.data })
-
-      } catch (error) {
-        console.log('CLIENT GET TASKS', error);
-      }
-  }
-
   render() {
     return (
       <section>
-        <form>
-            <textarea
-              value={this.state.newTweet}
-              onChange={this.handleInputChange}
-              onKeyDown={this.handleNewTweet}
-              placeholder="Hey, whats up?"
-            ></textarea>
-        </form>
-        
-      <ReactTable
-          className="-striped -highlight"
-          columns={columns}
-          data={this.state.tasks}
-          minRows= {this.state.tasks.length}
-        /> 
         </section>
     )
   }
