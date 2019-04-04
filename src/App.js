688d169054317e9e63cee7ae80fa55c3948f13ac
Andre Tonelli <aveigas@br.ibm.com>
2019-04-04 04:46:30 -0300

    Setup Api and Routes

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Routes from "./routes";
+import './styles/main.scss';
+
+const App = () => <Routes />;
+export default App;
---
+++ src/pages/Main/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import api from "../../services/api";
+  render() {
+    return (
+      <section>
+        </section>
+    )
+  }
+}
+
+export default withRouter(Main);
